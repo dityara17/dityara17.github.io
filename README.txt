db45ab563ec30cf5c8f803e83da71dc1882cf224
dityara17 <arizki717@gmail.com>
2017-07-19 14:31:18 +0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/towc/pen/ByVJre.
+
+ redid my pen [rainbow rain](http://codepen.io/MateiGCopot/pen/VYbYvQ)
--- js/index.js
+++ js/index.js
@@ -1,23 +1,56 @@
-// number of drops created.
-var nbDrop = 858; 
+    //initial
+var w = c.width = window.innerWidth,
+    h = c.height = window.innerHeight,
+    ctx = c.getContext('2d'),
+    
+    //parameters
+    total = (w/8)|0,
+    accelleration = .05,
+    lineAlpha = .02,
+    range = 25,
+    
+    //afterinitial calculations
+    size = w/total,
+    occupation = w/total,
+    repaintColor = 'rgba(0, 0, 0, .04)'
+    colors = [],
+    dots = [],
+    dotsVel = [];
 
-// function to generate a random number range.
-function randRange( minNum, maxNum) {
-  return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+//setting the colors' hue
+//and y level for all dots
+var portion = 360/total;
+for(var i = 0; i < total; ++i){
+  colors[i] = portion * i;
+  
+  dots[i] = h;
+  dotsVel[i] = 10;
 }
 
-// function to generate drops
-function createRain() {
-
-	for( i=1;i<nbDrop;i++) {
-	var dropLeft = randRange(0,1600);
-	var dropTop = randRange(-1000,1400);
-
-	$('.rain').append('<div class="drop" id="drop'+i+'"></div>');
-	$('#drop'+i).css('left',dropLeft);
-	$('#drop'+i).css('top',dropTop);
-	}
-
+function anim(){
+  window.requestAnimationFrame(anim);
+  
+  ctx.fillStyle = repaintColor;
+  ctx.fillRect(0, 0, w, h);
+  
+  for(var i = 0; i < total; ++i){
+    var currentY = dots[i] - 1;
+    dots[i] += dotsVel[i] += accelleration;
+    
+    for(var j = i+1; j < i+range && j < total; ++j){
+      if(Math.abs(dots[i] - dots[j]) <= range*size){
+        ctx.strokeStyle = 'hsla(hue, 80%, 50%, alp)'.replace('hue', (colors[i] + colors[j] + 360)/2 - 180).replace('alp', lineAlpha);
+        ctx.beginPath();
+        ctx.moveTo(i * occupation, dots[i]);
+        ctx.lineTo(j * occupation, dots[j]);
+        ctx.stroke();
+      }
+    }
+    
+    if(dots[i] > h && Math.random() < .01){
+      dots[i] = dotsVel[i] = 0;
+    }
+  }
 }
-// Make it rain
-createRain();
+
+anim();
