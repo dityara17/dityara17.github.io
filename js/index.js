20a256e7f18a5194ef92021a314b5be3312babf7
dityara17 <arizki717@gmail.com>
2017-10-11 19:33:55 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,184 +1,132 @@
-(function() {
-  var $animate, $container, $message, $paragraph, MESSAGES, animate, initialise, scramble;
-  document.oncontextmenu = document.body.oncontextmenu = function() {return false;}
-  document.onmousedown = document.body.onmousedown = function() {return false;}
-  MESSAGES = [];
+/* Real-Time Love Meter Experiment © Yogev Ahuvia */
+$(document).ready(function() {
+  $body = $('body');
+  $heart = $('.heart');
+  $counter = $('.lovers-count');
+  $lover = $('.lover');
 
-  MESSAGES.push({
-    delay: 1120,
-    text: "Halo, Fitria sayang..."
-  });
-
-  MESSAGES.push({
-    delay: 21200,
-    text: "Maaf, untuk sepatah kata yang tak mampu kuucapkan tiap aku salah."
-  });
-
-  MESSAGES.push({
-    delay: 32200,
-    text: "Terimakasih, untuk segala hal yang pernah kau lakukan untuk memerjuangkanku."
-  });
-
-  MESSAGES.push({
-    delay: 43600,
-    text: "Ketika setiap detik berlalu, tak pernah berubah perasaan sayangku kecuali benci."
-  });
-
-  MESSAGES.push({
-    delay: 55200,
-    text: "Dan apakah engkau mau mewujudkan mimpi yang sebelumnya telah kita rencanakan bersama?"
-  });
-
-  MESSAGES.push({
-    delay: 66400,
-    text: "Atau engkau memilih mundur dengan sejuta alasan tuk meninggalkanku?"
-  });
-
-  MESSAGES.push({
-    delay: 76400,
-    text: "Aku harap senyum dan janjimu tetap selama kian waktu"
-  });
-
-  MESSAGES.push({
-    delay: 86400,
-    text: "Bukankah itu yang kamu janjikan selalu kepada ku?"
-  });
-
-  MESSAGES.push({
-    delay: 96400,
-    text: "Tetaplah berusaha menepatinya meskipun sulit"
-  });
-
-  MESSAGES.push({
-    delay: 106400,
-    text: "Karena aku ada selama janjimu berlaku dan indahmu bersinar"
-  });
-
-  MESSAGES.push({
-    delay: 116400,
-    text: "I love you so much, porever."
-  });
+  updateLoversCount();
   
-  $container = $("#container");
+  setTimeout(function() {
+    $body.removeClass('preload');
+  }, 300);
+});
 
-  $message = $("#message");
-
-  $animate = $("#animate");
-
-  $paragraph = null;
-
-  scramble = function(element, text, options) {
-    var $element, addGlitch, character, defaults, ghostCharacter, ghostCharacters, ghostLength, ghostText, ghosts, glitchCharacter, glitchCharacters, glitchIndex, glitchLength, glitchProbability, glitchText, glitches, i, j, k, letter, object, order, output, parameters, ref, results, settings, shuffle, target, textCharacters, textLength, wrap;
-    defaults = {
-      probability: 0.2,
-      glitches: '-|/\\',
-      blank: '',
-      duration: text.length * 40,
-      ease: 'easeInOutQuad',
-      delay: 0.0
-    };
-    $element = $(element);
-    settings = $.extend(defaults, options);
-    shuffle = function() {
-      if (Math.random() < 0.5) {
-        return 1;
-      } else {
-        return -1;
+// fetche lovers data from server
+// update counter and bpm
+function updateLoversCount() {
+  $.getJSON(url, function(response) {
+    var $html = $(response.html);
+    var count = $html.find('.user-name').length;
+    
+    // handle +1, -1 or same lovers count
+    // load balance polling accordingly
+    if (count > lovers) {
+      polling /= 1.2;
+      lovers = count;
+      handleLatestLovers($html, count);
+      beat();
+    } else if (count < lovers) {
+      polling /= 1.2;
+      lovers = count;
+      
+      // clean previousLoverName when unloved
+      if (response.html.indexOf(previousLoverName) == -1) {
+        previousLoverName = '-1';
       }
-    };
-    wrap = function(text, classes) {
-      return "<span class=\"" + classes + "\">" + text + "</span>";
-    };
-    glitchText = settings.glitches;
-    glitchCharacters = glitchText.split('');
-    glitchLength = glitchCharacters.length;
-    glitchProbability = settings.probability;
-    glitches = (function() {
-      var j, len, results;
-      results = [];
-      for (j = 0, len = glitchCharacters.length; j < len; j++) {
-        letter = glitchCharacters[j];
-        results.push(wrap(letter, 'glitch'));
-      }
-      return results;
-    })();
-    ghostText = $element.text();
-    ghostCharacters = ghostText.split('');
-    ghostLength = ghostCharacters.length;
-    ghosts = (function() {
-      var j, len, results;
-      results = [];
-      for (j = 0, len = ghostCharacters.length; j < len; j++) {
-        letter = ghostCharacters[j];
-        results.push(wrap(letter, 'ghost'));
-      }
-      return results;
-    })();
-    textCharacters = text.split('');
-    textLength = textCharacters.length;
-    order = (function() {
-      results = [];
-      for (var j = 0; 0 <= textLength ? j < textLength : j > textLength; 0 <= textLength ? j++ : j--){ results.push(j); }
-      return results;
-    }).apply(this).sort(this.shuffle);
-    output = [];
-    for (i = k = 0, ref = textLength; 0 <= ref ? k < ref : k > ref; i = 0 <= ref ? ++k : --k) {
-      glitchIndex = Math.floor(Math.random() * (glitchLength - 1));
-      glitchCharacter = glitches[glitchIndex];
-      ghostCharacter = ghosts[i] || settings.blank;
-      addGlitch = Math.random() < glitchProbability;
-      character = addGlitch ? glitchCharacter : ghostCharacter;
-      output.push(character);
+      
+      setBeatTimer();
+    } else {
+      polling *= 1.2;
     }
-    object = {
-      value: 0
-    };
-    target = {
-      value: 1
-    };
-    parameters = {
-      duration: settings.duration,
-      ease: settings.ease,
-      step: function() {
-        var index, l, progress, ref1;
-        progress = Math.floor(object.value * (textLength - 1));
-        for (i = l = 0, ref1 = progress; 0 <= ref1 ? l <= ref1 : l >= ref1; i = 0 <= ref1 ? ++l : --l) {
-          index = order[i];
-          output[index] = textCharacters[index];
-        }
-        return $element.html(output.join(''));
-      },
-      complete: function() {
-        return $element.html(text);
-      }
-    };
-    return $(object).delay(settings.delay).animate(target, parameters);
-  };
+    
+    // update lovers count
+    $counter.text(lovers);
+    
+    // setTimeout for next poll on the server
+    setTimeout(function() {
+      updateLoversCount();
+    }, polling);
+  });
+}
 
-  animate = function() {
-    var data, element, index, j, len, options;
-    for (index = j = 0, len = MESSAGES.length; j < len; index = ++j) {
-      data = MESSAGES[index];
-      element = $paragraph.get(index);
-      element.innerText = '';
-      options = {
-        delay: data.delay
-      };
-      scramble(element, data.text, options);
+// resets timer with current bpm
+function setBeatTimer() {
+  beatTimer = setTimeout(function() {
+    beat();
+  }, bpm);
+}
+
+// parse latest lover data
+function handleLatestLovers($html, count) {
+  var lover = $html.find('.user-name')[count-1];
+  var loverAvatar = $(lover).find('img')[0];
+  var loverName = $.trim($(lover).text());
+  
+  if (loverName != previousLoverName) {
+    // if it's not the first fetch
+    // show Loved effect
+    if (previousLoverName != '') {
+      loved(loverName, loverAvatar);
     }
-  };
+    
+    previousLoverName = loverName;
+  }
+}
 
-  initialise = function() {
-    var index, j, len, text;
-    $animate.click(animate);
-    for (index = j = 0, len = MESSAGES.length; j < len; index = ++j) {
-      text = MESSAGES[index];
-      $message.append("<p>");
-    }
-    $paragraph = $container.find("p");
-    animate();
-  };
+// Heart Loved effect
+function loved(loverName, loverAvatar) {
+  $body.addClass('inlove');
 
-  initialise();
+  // update lover data in DOM
+  $lover.find('.avatar').html(loverAvatar);
+  $lover.find('.name').html(loverName);
+  
+  // clear stage for effect and do magic
+  $('.effect.wrapper').remove();
+  $heart.addClass('beat');
+  $lover.addClass('beat');
+  
+  // clear stage after transitions end
+  setTimeout(function() {
+    $heart.removeClass('beat');
+    $lover.removeClass('beat');
+    
+    setTimeout(function() {
+      $body.removeClass('inlove');
+    }, 2000);
+  }, 3000);
+}
 
-}).call(this);
+// Heart Beat effect
+function beat() {
+  bpm = (60/(lovers/2)) * 1000;
+
+  if (!$body.hasClass('inlove')) {
+    var animDuration = (bpm > 3000 ? 3000 : bpm);
+    
+    var $newWrapper = $('<div class="effect wrapper"></div');
+    var $newHeart = $('<i class="icon-heart beat"></i>');
+    $newHeart.css('animation-duration', animDuration + 'ms');
+    
+    // create shadow heart
+    $newWrapper.append($newHeart)
+    $('.main.wrapper').after($newWrapper);
+    
+    // setTimeout to remove the shadow heart later
+    setTimeout(function() {
+      $newWrapper.remove();
+    }, animDuration);
+  }
+    
+  setBeatTimer();
+}
+
+// a few helpers
+var $body, $heart, $counter, $lover;
+var lovers = 0;
+var polling = 1000;
+var bpm = 0;
+var beatTimer = null;
+var previousLoverName = '';
+var url = "http://kindofone.me/services/codepen-lovers.php";
